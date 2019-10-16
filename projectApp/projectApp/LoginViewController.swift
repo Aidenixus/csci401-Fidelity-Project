08b5f4964526bdc50fff2f2100856c7b0dfdb8f5
Hiro Zhu <hirozhu@Hiros-MacBook-Pro.local>
2019-10-16 02:26:20 -0700

    added login and signup frontend functionality and alert box for wrong password

---
+++ projectApp/projectApp/LoginViewController.swift
@@ -0,0 +1,81 @@
+//
+//  LoginScreen.swift
+//  projectApp
+//
+//  Created by Minxuan Song on 10/10/19.
+//  Copyright © 2019 Minxuan Song. All rights reserved.
+//
+
+import UIKit
+
+class User {
+    var username: String
+    var password: String
+    
+    init(username: String, password: String){
+        self.username = username;
+        self.password = password;
+    }
+}
+
+let dummyUser: User = User(username: "xubozhu@usc.edu", password: "123")
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBAction func didTapLogin(_ sender: UIButton) {
+        print("username: ", usernameTextField.text!)
+        print("password: ", passwordTextField.text!)
+        verifyLogin(username: usernameTextField.text!, password: passwordTextField.text!)
+    }
+    @IBAction func didTapSignUp(_ sender: UIButton) {
+    }
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    func verifyLogin(username: String, password: String)
+    {
+        
+        let alert = UIAlertController(title: "Wrong Password, please try again.", message: nil, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: { (action) in alert.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        //  do query of username to password to backend now
+        
+        // do verification based on the username/password fetched from backend
+        var verified = false;
+        if (username == dummyUser.username && password == dummyUser.password){
+            verified = true;
+        }
+        
+        if (verified)
+        {
+            // jump into the profile picture
+        }
+        else
+        {
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
